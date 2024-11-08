08fdfe3442cd7daf45e2ea1768c2398476d89fab
TheLastOne001 <andikaarifsofyan1419@gmail.com>
2024-11-08 23:37:12 +0700

    first commit

---
+++ lib/data/data.dart
@@ -0,0 +1,81 @@
+String f = "assets/"; // f for folder
+String avatarSrc = "${f}toji.jpg";
+List<Map<String, dynamic>> data = [
+  {
+    "name": "AC Valhalla",
+    "src": "${f}ac_valhalla.png",
+    "favorite": false,
+  },
+  {
+    "name": "HDMI Cable",
+    "src": "${f}hdmi_cable.png",
+    "favorite": false,
+  },
+  {
+    "name": "NBA2K23",
+    "src": "${f}nba2k23.png",
+    "favorite": false,
+  },
+  {
+    "name": "Nuclear Blaze",
+    "src": "${f}nuclear_blaze.png",
+    "favorite": false,
+  },
+  {
+    "name": "PS Camera",
+    "src": "${f}ps_camera.png",
+    "favorite": false,
+  },
+  {
+    "name": "PS Pulse Earphone",
+    "src": "${f}ps_pulse_earphone.png",
+    "favorite": false
+  },
+  {"name": "PS Vita 2", "src": "${f}ps_vita_2.png", "favorite": false},
+  {
+    "name": "PS4 Controller",
+    "src": "${f}ps4_controller.png",
+    "favorite": false
+  },
+  {"name": "PS4 Slim Pro", "src": "${f}ps4_slim_pro.png", "favorite": false},
+  {
+    "name": "PS5 Charging Station",
+    "src": "${f}ps5_charging_station.png",
+    "favorite": false
+  },
+  {
+    "name": "PS5 Controller",
+    "src": "${f}ps5_controller.png",
+    "favorite": false,
+  },
+  {
+    "name": "PS5 Digital Edition",
+    "src": "${f}ps5_digital_edition.png",
+    "favorite": false
+  },
+  // {
+  //   "name": "PS5 Remote",
+  //   "src": "${f}ps_remote.webp",
+  //   "favorite": false,
+  // },
+  {
+    "name": "Pulse Headset",
+    "src": "${f}pulse_headset_ps5.png",
+    "favorite": false,
+  },
+  // {
+  //   "name": "Sony Headphone",
+  //   "src": "${f}sony_headphone.png",
+  //   "favorite": false
+  // },
+  {
+    "name": "Spider-Man 2",
+    "src": "${f}spider_man_2.png",
+    "favorite": false,
+  },
+  {
+    "name": "Tekken 7",
+    "src": "${f}tekken7.png",
+    "favorite": false,
+  }
+];
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:game_store/components/carousel/carousel.dart';
+import 'package:game_store/components/appbar/custom_appbar.dart';
+import 'package:game_store/components/categories/categories.dart';
+import 'package:game_store/components/products/products_list_grid.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                const CustomAppbar(),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    Text(
+                      "Hello",
+                      style: GoogleFonts.poppins(),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Andika Arif Sofyan",
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Carousel(),
+                const SizedBox(height: 20),
+                const Categories(),
+                const SizedBox(height: 20),
+
+                // Products
+                // Let's try something !
+                const SizedBox(height: 400, child: ProductGrid()),
+                const SizedBox(height: 15),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
